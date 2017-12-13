2a155da91e7f4b13ffea6404ff1c08842ef58269
junli1014 <1014gin@gmail.com>
2017-12-13 17:10:47 -0500

    create discovery branch

--- assignforce-discovery/src/test/java/com/revature/assignforcediscovery/AssignforceDiscoveryApplicationTests.java
+++ assignforce-discovery/src/test/java/com/revature/assignforcediscovery/AssignforceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.revature.assignforcegateway;
+package com.revature.assignforcediscovery;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AssignforceGatewayApplicationTests {
+public class AssignforceDiscoveryApplicationTests {
 
 	@Test
 	public void contextLoads() {
