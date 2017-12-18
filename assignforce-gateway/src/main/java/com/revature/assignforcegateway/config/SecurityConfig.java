5267b03d82bae74be6925380b94d3b239abd0735
Max Wang <maxwizdomer@gmail.com>
2017-12-17 19:17:44 -0500

    added cors and redirect to security

--- assignforce-gateway/src/main/java/com/revature/assignforcegateway/config/SecurityConfig.java
+++ assignforce-gateway/src/main/java/com/revature/assignforcegateway/config/SecurityConfig.java
@@ -6,6 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
 
 /**
  * Created by Marquis on 7/12/2017.
@@ -14,14 +21,33 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// require https
 	    http.requiresChannel().antMatchers("/**").requiresSecure().and().portMapper().http(80).mapsTo(443);
 	    
 	    http.antMatcher("/**").authorizeRequests().anyRequest().authenticated().and()
+		.cors().and()
 		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 		    
 		    //.and().formLogin().loginPage("/").loginProcessingUrl("/login").defaultSuccessUrl("/home");
 	}
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(new String[]{"http://localhost:4200"}));
+        configuration.setAllowedMethods(Arrays.asList(new String[]{"HEAD",
+		    "GET", "POST", "PUT", "DELETE", "PATCH"}));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(Arrays.asList(new String[]{"Cache-Control", "Content-Type", "X-XSRF-TOKEN"}));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- assignforce-gateway/src/main/java/com/revature/assignforcegateway/controllers/AuthController.java
+++ assignforce-gateway/src/main/java/com/revature/assignforcegateway/controllers/AuthController.java
@@ -4,6 +4,7 @@ package com.revature.assignforcegateway.controllers;
 // import io.swagger.annotations.ApiOperation;
 // import io.swagger.annotations.ApiResponse;
 // import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
@@ -13,24 +14,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class AuthController{
 
+    @Value("${frontEndUrl}")
+    private static String frontEndUrl;
+    
     @RequestMapping(value= "/auth/userinfo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Principal> userInfo(Principal principal){
 	return new ResponseEntity<Principal>(principal, HttpStatus.OK);
     }
     
     @RequestMapping(value= "/login**")
-    public ResponseEntity login(Principal principal){
+    public ResponseEntity login(){
 	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @RequestMapping(value= "/home", method = RequestMethod.GET)
-    public ResponseEntity home(Principal principal){
-	return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @RequestMapping(value= "/", method = RequestMethod.GET)
+    public void home(HttpServletResponse response) throws IOException{
+	response.sendRedirect("http://localhost:4200/overview");
     }
-   
     
 }
