0ddb058684b82f7a5068ccb06b8fdec32ec0a189
junli1014 <1014gin@gmail.com>
2017-12-27 09:33:32 -0500

    controller function of curriculum service tested

--- curriculumService/src/main/java/com/revature/curriculumservice/CurriculumServiceApplication.java
+++ curriculumService/src/main/java/com/revature/curriculumservice/CurriculumServiceApplication.java
@@ -2,11 +2,13 @@ package com.revature.curriculumservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class CurriculumServiceApplication {
 
 	public static void main(String[] args) {
