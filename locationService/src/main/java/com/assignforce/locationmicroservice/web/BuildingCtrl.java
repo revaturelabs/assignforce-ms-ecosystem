4744d43740f80fe464a81234c7836eefd63938c8
sggeniussg <jeffreywu0216@gmail.com>
2017-12-30 15:01:46 -0500

    add temp cross origin solution to ctrl

--- locationService/src/main/java/com/assignforce/locationmicroservice/web/BuildingCtrl.java
+++ locationService/src/main/java/com/assignforce/locationmicroservice/web/BuildingCtrl.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping("/api/v2/building")
 @ComponentScan(basePackages = "com.assignforce.locationmicroservice.service")
--- locationService/src/main/java/com/assignforce/locationmicroservice/web/RoomCtrl.java
+++ locationService/src/main/java/com/assignforce/locationmicroservice/web/RoomCtrl.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping("/api/v2/room")
 @ComponentScan(basePackages="com.assignforce.locationmicroservice.service")
