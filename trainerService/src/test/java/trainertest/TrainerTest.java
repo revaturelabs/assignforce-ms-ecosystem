fac53f15b8eedb067af7fc54af7ba3d7b57867d3
kan <kanxue0012@gmail.com>
2018-01-03 15:52:29 -0500

    fixed delete, find, and update

--- trainerService/src/test/java/trainertest/TrainerTest.java
+++ trainerService/src/test/java/trainertest/TrainerTest.java
@@ -134,7 +134,7 @@ public class TrainerTest {
     @Test
 //    @WithMockUser(roles="ADMIN")
     public void retrieveTrainerTest() throws Exception{
-        given(trainerService.getOneItem(any(Integer.class))).willReturn(testTrainer);
+        given(trainerService.getOneItem(any(String.class))).willReturn(testTrainer);
         mvc.perform(get("/api/v2/trainer/42")
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
@@ -146,7 +146,7 @@ public class TrainerTest {
     @Test
 //    @WithMockUser(roles="ADMIN")
     public void retrieveTrainerWithBadIdTest() throws Exception{
-        given(trainerService.getOneItem(any(Integer.class))).willReturn(null);
+        given(trainerService.getOneItem(any(String.class))).willReturn(null);
         mvc.perform(get("/api/v2/trainer/42")
                 .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isNotFound());
@@ -187,7 +187,7 @@ public class TrainerTest {
     @Test
 //    @WithMockUser(roles="ADMIN")
     public void deleteTrainerTest() throws Exception{
-        doNothing().when(trainerService).deleteItem(any(Integer.class));
+        doNothing().when(trainerService).deleteItem(any(String.class));
         mvc.perform(delete("/api/v2/trainer/42"))
                 .andExpect(status().isOk());
     }
