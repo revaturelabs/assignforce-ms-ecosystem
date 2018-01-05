f5bcb04763298ba440a9269a9942adaa2d61e5cf
junli1014 <1014gin@gmail.com>
2018-01-04 22:17:27 -0500

    add documentation for skill and curriculum service

---
+++ Documentation/CurriculumSequence.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Tuesday-December-26-2017   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence CURRICULUM_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ASSIGNFORCE_DEV"."CURRICULUM_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 61 CACHE 20 NOORDER  NOCYCLE   ;
