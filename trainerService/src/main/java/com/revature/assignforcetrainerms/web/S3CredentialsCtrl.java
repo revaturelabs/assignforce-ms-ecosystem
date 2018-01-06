c9d8eee57d0d87ff09bdffbab91ec8f7c3bd881e
kan <kanxue0012@gmail.com>
2018-01-05 22:37:43 -0500

    added cross origin

--- trainerService/src/main/java/com/revature/assignforcetrainerms/web/S3CredentialsCtrl.java
+++ trainerService/src/main/java/com/revature/assignforcetrainerms/web/S3CredentialsCtrl.java
@@ -11,10 +11,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 //import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/v2/s3Creds")
 @ComponentScan(basePackages="com.revature.assignforce.service")
