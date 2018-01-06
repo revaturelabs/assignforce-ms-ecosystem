17c157c0117972277bb1ac3f482e0f99cbee47f7
Joseph <joseph.wong.qc@gmail.com>
2018-01-06 12:57:32 -0500

    use PCF mlab service

--- trainerService/src/main/java/com/revature/assignforcetrainerms/AssignforceTrainerMsApplication.java
+++ trainerService/src/main/java/com/revature/assignforcetrainerms/AssignforceTrainerMsApplication.java
@@ -1,15 +1,21 @@
 package com.revature.assignforcetrainerms;
 
+import com.revature.assignforcetrainerms.config.SpringApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class AssignforceTrainerMsApplication {
+public class AssignforceTrainerMsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssignforceTrainerMsApplication.class, args);
+		new SpringApplicationBuilder(AssignforceTrainerMsApplication.class).
+				initializers(new SpringApplicationContextInitializer())
+				.application()
+				.run(args);
 	}
 }
