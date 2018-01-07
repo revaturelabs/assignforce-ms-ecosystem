0f96e074469325b34b635cbd82577263180971b3
Junjie Wang <junjie2412@gmail.com>
2018-01-06 20:52:34 -0500

    pcf configurations

---
+++ batchService/src/main/java/com/revature/assignforcebatchms/dao/BatchLocationRep.java
@@ -0,0 +1,9 @@
+package com.revature.assignforcebatchms.dao;
+
+import com.revature.assignforcebatchms.domain.BatchLocation;
+
+/**
+ * Created by August Duet on 4/7/2017.
+ */
+public interface BatchLocationRep extends BaseRepository<BatchLocation, Integer>{
+}
