45bbce70b4f116c62a5472aed775998faff748e6
junli1014 <1014gin@gmail.com>
2018-01-08 11:05:28 -0500

    remove CORS

--- curriculumService/src/main/java/com/revature/curriculumservice/web/CurriculumCtrl.java
+++ curriculumService/src/main/java/com/revature/curriculumservice/web/CurriculumCtrl.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+// @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v2/curriculum")
 @ComponentScan(basePackages="com.revature.curriculumservice.service")
