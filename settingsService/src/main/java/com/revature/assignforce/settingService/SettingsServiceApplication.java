756163ee6d851ea52e0071429624d21fb41b98cc
Joseph <joseph.wong.qc@gmail.com>
2018-01-08 15:49:14 -0500

    add security

--- settingsService/src/main/java/com/revature/assignforce/settingService/SettingsServiceApplication.java
+++ settingsService/src/main/java/com/revature/assignforce/settingService/SettingsServiceApplication.java
@@ -2,6 +2,9 @@ package com.revature.assignforce.settingService;
 
 import com.revature.assignforce.settingService.config.SpringApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,7 +13,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @EnableMongoRepositories(basePackages = "com.revature.assignforce.settingService")
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.revature.assignforce.settingService", "com.revature.assignforcecommon.security"},
+		exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 //@EnableDiscoveryClient
 public class SettingsServiceApplication extends SpringBootServletInitializer {
 
