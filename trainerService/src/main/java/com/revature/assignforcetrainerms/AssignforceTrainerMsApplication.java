aa7bc443bd70d540dd6ae4ed49c02a427a7641a7
Joseph <joseph.wong.qc@gmail.com>
2018-01-08 00:53:59 -0500

    add in security

--- trainerService/src/main/java/com/revature/assignforcetrainerms/AssignforceTrainerMsApplication.java
+++ trainerService/src/main/java/com/revature/assignforcetrainerms/AssignforceTrainerMsApplication.java
@@ -3,12 +3,16 @@ package com.revature.assignforcetrainerms;
 import com.revature.assignforcetrainerms.config.SpringApplicationContextInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.revature.assignforcetrainerms", "com.revature.assignforcecommon.security"},
+		exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableEurekaClient
 public class AssignforceTrainerMsApplication extends SpringBootServletInitializer {
 
