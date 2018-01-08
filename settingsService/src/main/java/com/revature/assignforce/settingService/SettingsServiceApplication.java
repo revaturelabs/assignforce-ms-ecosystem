dd8fb4ee9e9bf775819f60484785e5dc0bb80d04
Jeremy Shapiro <jrmy831@gmail.com>
2018-01-08 10:47:47 -0500

    lets get this working again

---
+++ settingsService/src/main/java/com/revature/assignforce/settingService/SettingsServiceApplication.java
@@ -0,0 +1,25 @@
+package com.revature.assignforce.settingService;
+
+import com.revature.assignforce.settingService.config.SpringApplicationContextInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@EnableMongoRepositories(basePackages = "com.revature.assignforce.settingService")
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class SettingsServiceApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder( SettingsServiceApplication.class).
+			initializers( new SpringApplicationContextInitializer())
+			.application()
+			.run( args);
+
+//		SpringApplication.run(SettingsServiceApplication.class, args);
+	}
+}
